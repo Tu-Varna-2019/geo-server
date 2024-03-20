ac9bc7d97eb19b2b61461171a7e1d2c8139eefb4
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2024-03-20 21:59:28 +0200

    Add mapstruct-processor dependency

--- src/main/java/com/tuvarna/geo/service/UserServiceImpl.java
+++ src/main/java/com/tuvarna/geo/service/UserServiceImpl.java
@@ -24,18 +24,22 @@ public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
     private UserValidateService userValidateService;
+    private UserMapper userMapper;
 
     private BCryptPasswordEncoder encodePassword;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository, UserTypeRepository userTypeRepository,
-            UserValidateService userValidateService, UserMapper userMapper) {
+            UserValidateService userValidateService, UserMapper userMapper, BCryptPasswordEncoder encodePassword) {
         this.userRepository = userRepository;
         this.userValidateService = userValidateService;
+        this.userMapper = userMapper;
+        this.encodePassword = encodePassword;
     }
 
     @Override
     @Transactional
+    @SuppressWarnings("squid:S3457")
     public RestApiResponse<Void> registerUser(RegisterUserDTO userDto) {
 
         User user = userMapper.toEntity(userDto, encodePassword);
@@ -43,11 +47,11 @@ public class UserServiceImpl implements UserService {
 
         // Check if the user already exists
         userValidateService.validateUserDoesNotExist(userDto.getEmail());
-        logger.info("User %s does not exist", userDto.getEmail());
+        logger.info("User does not exist with given email: " + userDto.getEmail());
 
         // Check if the user type exists
         user.setUserType(userValidateService.validateUserTypeExists(userDto.getUsertype()));
-        logger.info("User type %s exists", userDto.getUsertype());
+        logger.info("User type exists: " + userDto.getUsertype());
 
         userRepository.save(user);
 
@@ -58,14 +62,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
+    @SuppressWarnings("squid:S3457")
     public RestApiResponse<Void> authenticateUser(LoginUserDTO userDto) {
 
-        User user = userMapper.toEntity(userDto, encodePassword);
+        User user = userMapper.toEntity(userDto);
         logger.info("Mapping user DTO to entity");
 
         // Fetch the user from the database
         userValidateService.validateUserExists(user.getEmail());
-        logger.info("User %s does not exist", userDto.getEmail());
+        logger.info("User exist with given email: " + userDto.getEmail());
 
         User userFromDb = userRepository.findByEmail(user.getEmail());
 
