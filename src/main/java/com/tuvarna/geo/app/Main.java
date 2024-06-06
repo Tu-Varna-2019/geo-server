f2fba304dc0414ab94a411d540d5131afe9c76cf
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2024-06-06 15:04:10 +0300

    ➕ Fix: nvim is now able to run springboot

--- src/main/java/com/tuvarna/geo/app/Main.java
+++ src/main/java/com/tuvarna/geo/app/Main.java
@@ -1,10 +1,10 @@
 package com.tuvarna.geo.app;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.tuvarna.geo")
@@ -14,6 +14,5 @@ public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
-
 	}
 }
