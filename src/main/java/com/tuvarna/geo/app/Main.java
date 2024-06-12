0d86b8207cff18427637c7ccd4f6f0ff9d012dc5
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2024-06-12 22:22:06 +0300

    🐛 Fix: non-vscode IDEs are now able to run

--- src/main/java/com/tuvarna/geo/app/Main.java
+++ src/main/java/com/tuvarna/geo/app/Main.java
@@ -1,10 +1,10 @@
 package com.tuvarna.geo.app;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.tuvarna.geo")
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan(basePackages = "com.tuvarna.geo.entity")
 public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
 }
